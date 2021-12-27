6241c98824076e76321711742912e727157b071f
Zia <ghezal-ahmad.zia@bam.de>
2021-12-27 12:53:32 +0100

    deleting old files before updated project

--- app.py
+++ app.py
@@ -1,63 +1,154 @@
-from flask import Flask,render_template,request
-from werkzeug.utils import secure_filename
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request, redirect, make_response, send_file
+import os
 import pandas as pd
-import csv
+import numpy as np
+import models as algorithms
+import ploting as plotfun
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from io import BytesIO
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+
+bootstrap = Bootstrap(app)
+
+def datasetList():
+    datasets = [x.split('.')[0] for f in ['datasets', 'preprocessed'] for x in os.listdir(f)]
+    extensions = [x.split('.')[1] for f in ['datasets', 'preprocessed'] for x in os.listdir(f)]
+    folders = [f for f in ['datasets', 'preprocessed'] for x in os.listdir(f)]
+    return datasets, extensions, folders
+
+#Load columns of the dataset
+def loadColumns(dataset):
+    datasets, extensions, folders = datasetList()
+    if dataset in datasets:
+        extension = extensions[datasets.index(dataset)]
+        if extension == 'txt':
+            df = pd.read_table(os.path.join(folders[datasets.index(dataset)], dataset + '.txt'), nrows=0)
+        elif extension == 'csv':
+            df = pd.read_csv(os.path.join(folders[datasets.index(dataset)], dataset + '.csv'), nrows=0)
+        return df.columns
+
+#Load Dataset
+def loadDataset(dataset):
+    datasets, extensions, folders = datasetList()
+    if dataset in datasets:
+        extension = extensions[datasets.index(dataset)]
+        if extension == 'txt':
+            df = pd.read_table(os.path.join(folders[datasets.index(dataset)], dataset + '.txt'))
+        elif extension == 'csv':
+            df = pd.read_csv(os.path.join(folders[datasets.index(dataset)], dataset + '.csv'))
+        return df
 
 
-# textarea that accept csv format text
-@app.route('/', methods=['GET', 'POST'])
-def root():
-    if request.method == 'GET':
-        return render_template('home.html')
-    elif request.method == 'POST':
-        results = []
-        user_csv = request.form.get('user_csv').split('\n')
-        reader = csv.DictReader(user_csv)
-        for row in reader:
-            results.append(dict(row))
-        fieldnames = [key for key in results[0].keys()]
-        return render_template('home.html', results=results, fieldnames=fieldnames, len=len)
-
-
-
-
-
-
-# uploading file 
-@app.route('/upload',methods = ['GET','POST'])
-def upload_route_summary():
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    datasets,_,folders = datasetList()
+    originalds = []
+    featuresds = []
+    for i in range(len(datasets)):
+        if folders[i] == 'datasets': originalds += [datasets[i]]
+        else: featuresds += [datasets[i]]
     if request.method == 'POST':
-            # Create variable for uploaded file
-        file = request.files['file']
-        file.save(os.path.join("uploaded", file.filename))
-        return render_template('upload.html', message='uploaded successfully')
-    return render_template('upload.html', message='upload')
+            f = request.files['file']
+            f.save(os.path.join('datasets', f.filename))
+            return redirect('/')
+    return render_template('index.html', originalds = originalds, featuresds = featuresds)
 
+@app.route('/datasets/')
+def datasets():
+    return redirect('/')
 
-
-
-# Invalid url
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-
-
-
-
-
-
-#df = pd.read_csv('uploaded/Simp_Embedded_Data_28d_cubic_strength.csv')
-#print(df.head())
-if __name__ == '__main__':
-    import os
-    HOST = os.environ.get("SERVER_HOST", 'localhost')
+@app.route('/datasets/<dataset>')
+def dataset(description = None, head = None, dataset = None):
+    df = loadDataset(dataset)
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT=5555
-    app.run(debug=True)
+        description = df.describe().round(2)
+        head = df.head(5)
+    except: pass
+    return render_template('dataset.html',
+                           description = description.to_html(classes='table table-striped table-hover'),
+                           head = head.to_html(index=False, classes='table table-striped table-hover'),
+                           dataset = dataset)
+
+
+
+@app.route('/datasets/<dataset>/preprocessing')
+def preprocessing(dataset = dataset):
+    columns = loadColumns(dataset)
+    return render_template('preprocessing.html', dataset = dataset, columns=columns)
+
+@app.route('/datasets/<dataset>/preprocessed_dataset/', methods=['POST'])
+def preprocessed_dataset(dataset):
+    numFeatures = request.form.get('nfeatures')
+    manualFeatures = request.form.getlist('manualfeatures')
+    datasetName = request.form.get('newdataset')
+    response = request.form.get('response')
+    dropsame = request.form.get('dropsame')
+    dropna = request.form.get('dropna')
+
+    df = loadDataset(dataset)
+
+    if dropna == 'all':
+        df = df.dropna(axis=1, how='all')
+    elif dropna == 'any':
+        df.dropna(axis=1, how='any')
+
+    filename = dataset + '_'
+    try:
+        nf = int(numFeatures)
+        from sklearn.feature_selection import SelectKBest, chi2
+        X = df.drop(str(response), axis=1)
+        y = df[str(response)]
+        kbest = SelectKBest(chi2, k=nf).fit(X,y)
+        mask = kbest.get_support()
+        # List of K best features
+        best_features = []
+        for bool, feature in zip(mask, list(df.columns)):
+            if bool: best_features.append(feature)
+        #Reduced Dataset
+        df = pd.DataFrame(kbest.transform(X),columns=best_features)
+        df.insert(0, str(response), y)
+
+        filename += numFeatures + '_' + 'NA' + dropna + '_Same' + dropsame + '.csv'
+
+    except:
+        df = df[manualFeatures]
+        filename += str(datasetName) + '_' + str(response) + '.csv'
+
+    if dropsame == 'Yes':
+        nunique = df.apply(pd.Series.nunique)
+        cols_to_drop = nunique[nunique == 1].index
+        df = df.drop(cols_to_drop, axis=1)
+    df.to_csv(os.path.join('preprocessed', filename), index=False)
+    return redirect('/datasets/' + filename.split('.')[0])
+
+
+@app.route('/datasets/<dataset>/graphs')
+def graphs(dataset = dataset):
+    columns = loadColumns(dataset)
+    return render_template('graphs.html', dataset = dataset, columns=columns)
+
+@app.route('/datasets/<dataset>/graphprocess/', methods=['POST'])
+def graph_process(dataset = dataset):
+    histogram = request.form.getlist('histogram')
+    boxplotcat = request.form.get('boxplotcat')
+    boxplotnum = request.form.get('boxplotnum')
+    corr = request.form.getlist('corr')
+    corrcat = request.form.get('corrcat')
+
+    if corrcat != '': corr += [corrcat]
+    ds = loadDataset(dataset)
+    import plotfunctions as plotfun
+    figs = {}
+    if histogram != [''] and histogram != []:
+        figs['Histograms'] = str(plotfun.plot_histsmooth(ds, histogram), 'utf-8')
+    if corr != [''] and corr != []:
+        figs['Correlations'] = str(plotfun.plot_correlations(ds, corr, corrcat), 'utf-8')
+    if boxplotcat != '' and boxplotnum != '':
+        figs['Box Plot'] = str(plotfun.plot_boxplot(ds, boxplotcat, boxplotnum), 'utf-8')
+    if figs == {}: return redirect('/datasets/' + dataset + '/graphs')
+    return render_template('drawgraphs.html', figs = figs, dataset = dataset)
+
+if __name__ == '__main__':
+    app.run(debug=False)
